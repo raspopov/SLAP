b0c9a4f29fcd9cbee6beff58b726be0da6c1466b
raspopov <raspopov@cherubicsoft.com>
2021-10-19 06:59:50 +0300

    Using pre-compiled bakefile.

--- UpdateWinSparkle.cmd
+++ UpdateWinSparkle.cmd
@@ -1,2 +1,3 @@
-..\bakefile\src\tool.py -v -t vs2017 WinSparkle\WinSparkle.bkl
+rem https://github.com/vslavik/bakefile/releases
+..\bakefile-1.2.6-win\bkl -v -t vs2017 WinSparkle\WinSparkle.bkl
 @pause
